bc3ff0cf29eb98dabc0f33e83b956b0161f8ef3f
TongXiong <tongxiong.9901@gmail.com>
2023-06-14 23:35:37 -0700

    changes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -33,7 +33,7 @@ router.get("/current", restoreUser, requireAuth, async (req, res) => {
             }
         ],
         attributes: ["id", "userId", "spotId", "review", "stars", "createdAt", "updatedAt"],
-        group: ["Review.id", "User.id", "Spot.id"]
+        group: ["Review.id", "User.id", "Spot.id", "ReviewImages.url"]
     })
     res.json({
         reviews
